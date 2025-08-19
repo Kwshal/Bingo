776485c3a5ce6487b892ec5b26f0c3423c4c9f46
Kushal <kushal3738@gmail.com>
2025-08-19 08:28:39 +0530

    ..

--- db.js
+++ db.js
@@ -9,7 +9,7 @@
     apiKey: "AIzaSyABxgXrl0ZawYX4U_Bqpt9HRVgSaW1CA7k",
     authDomain: "bingo-ace.firebaseapp.com",
     projectId: "bingo-ace",
-    databaseID: "https://bingo-ace-default-rtdb.asia-southeast1.firebasedatabase.app"
+    databaseURL: "https://bingo-ace-default-rtdb.asia-southeast1.firebasedatabase.app"
     storageBucket: "bingo-ace.firebasestorage.app",
     messagingSenderId: "932968616436",
     appId: "1:932968616436:web:dbe017a0a451dcfde329a1"
