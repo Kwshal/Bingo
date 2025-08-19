a92fbe064308d4370783db731e77baf68c239f37
Kushal <kushal3738@gmail.com>
2025-08-19 12:11:06 +0530

    Fix import error

--- db.js
+++ db.js
@@ -1,34 +1,33 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp, onValue, set, ref } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyABxgXrl0ZawYX4U_Bqpt9HRVgSaW1CA7k",
-    authDomain: "bingo-ace.firebaseapp.com",
-    projectId: "bingo-ace",
-    databaseURL: "https://bingo-ace-default-rtdb.asia-southeast1.firebasedatabase.app",
-    storageBucket: "bingo-ace.firebasestorage.app",
-    messagingSenderId: "932968616436",
-    appId: "1:932968616436:web:dbe017a0a451dcfde329a1"
-  };
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABxgXrl0ZawYX4U_Bqpt9HRVgSaW1CA7k",
+  authDomain: "bingo-ace.firebaseapp.com",
+  projectId: "bingo-ace",
+  databaseURL: "https://bingo-ace-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "bingo-ace.firebasestorage.app",
+  messagingSenderId: "932968616436",
+  appId: "1:932968616436:web:dbe017a0a451dcfde329a1"
+};
 
 const p = document.getElementById("sharedText");
 
-  // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app)
-let txtRef = ref(db, "cutNum")
+const db = getDatabase(app);
+const txtRef = ref(db, "cutNum");
 
-let pUpdater = (numList) => set(txtRef, numList)
+// Writer function
+const pUpdater = (numList) => set(txtRef, numList);
 
-onValue(txtRef, snapshot => {
+// Listener
+onValue(txtRef, (snapshot) => {
   if (snapshot.exists()) {
     p.innerText = snapshot.val();
   }
 });
 
-export {pUpdater}
-
+export { pUpdater };
