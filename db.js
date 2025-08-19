cacfb94c2006d1e9f94b31dcf676e9eaff1dc12d
Kushal <kushal3738@gmail.com>
2025-08-19 08:05:10 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyABxgXrl0ZawYX4U_Bqpt9HRVgSaW1CA7k",
+    authDomain: "bingo-ace.firebaseapp.com",
+    projectId: "bingo-ace",
+    storageBucket: "bingo-ace.firebasestorage.app",
+    messagingSenderId: "932968616436",
+    appId: "1:932968616436:web:dbe017a0a451dcfde329a1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
